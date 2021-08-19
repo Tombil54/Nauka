1c99fcea404a06c9a010ee7048e946297c529498
TomBil54 <tomasz.bilski@gmail.com>
2021-08-19 09:37:58 +0200

    I'm adding error handling for an input variable

--- zadanie1.py
+++ zadanie1.py
@@ -1,5 +1,8 @@
-n = int(input("podaj podstawę: "))
-silnia = 1
-for x in range(1,n+1):
-    silnia = silnia * x
-print(silnia)
+try:
+    basic = int(input("podaj podstawę: "))
+    silnia = 1
+    for x in range(2,basic+1):
+        silnia = silnia * x
+    print(silnia)
+except:
+    print("Nieprawidłowa wartość!!!")
